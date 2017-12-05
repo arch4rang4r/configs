7f0064c772d47f42b9c11056ad1369cdbd5d8236
arch4rang4r <arch4rang4r@users.noreply.github.com>
2017-12-04 19:04:01 -0500

    Update status bar

--- i3/mybar.py
+++ i3/mybar.py
@@ -14,17 +14,12 @@ status.register(
     format = 'BAT: [{status} ]{percentage:.2f}%[ {remaining} remaining]',
     not_present_text = '',
     status = {
-        'DPL': 'DPL', # What actually is this? I don't think I've ever seen it show up
+        'DPL': 'DPL',
         'CHR': 'Charging',
         'DIS': 'Discharging',
         'FULL': '',
     }
 )
-status.register(
-    'dpms',
-    format = 'DPMS: on', # Workaround for #537
-    format_disabled = 'DPMS: off'
-)
-status.register('window_title')
+status.register('dpms')
 
 status.run()
